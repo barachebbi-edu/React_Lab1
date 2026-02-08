a6f0d79f340a805063a90e236c777ea4c8db2768
Bara Edu <bara.chebbi@horizon-university.tn>
2026-02-08 16:46:22 +0100

    added the StatusBadge component to the App.jsx file and fixed some errors in the JSXErrors.jsx file.

--- lab1/lab-1-fundamentals/src/App.jsx
+++ lab1/lab-1-fundamentals/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Greeting from './components/Greeting'
+import Greeting from './components/Greeting' //importing the Greeting component
+import StatusBadge from './components/StatusBadge' //importing the StatusBadge component
 
 function App() {
   return (
     <div className="App">
       <Greeting />
+      <StatusBadge />
     </div>
   )
-
 }
 
 export default App;
--- lab1/lab-1-fundamentals/src/components/StatusBadge.jsx
+++ lab1/lab-1-fundamentals/src/components/StatusBadge.jsx
@@ -1,1 +1,16 @@
-function 
+function StatusBadge() {
+  const isOnline = true 
+
+  return (
+    <div>
+      <h3 style={{ color: isOnline ? "green" : "red" }}>
+        {isOnline ? "🟢 On" : "🔴 Off"}
+      </h3>
+      <p>
+        User is currently {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+  )
+}
+
+export default StatusBadge
